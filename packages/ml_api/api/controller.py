fe6ab4503f5ba8998f5a496bb39fbc3476a8d6a5
celest <cnaoucy@gmail.com>
2025-12-19 12:11:08 -0500

    Ajout validation du schéma d'API

--- packages/ml_api/api/controller.py
+++ packages/ml_api/api/controller.py
@@ -3,6 +3,7 @@ from regression_model.predict import make_prediction
 from regression_model import __version__ as _version
 
 from api.config import get_logger
+from api.validation import validate_inputs
 from api import __version__ as api_version
 
 _logger = get_logger(logger_name=__name__)
@@ -28,14 +29,20 @@ def version():
 @prediction_app.route('/v1/predict/regression', methods=['POST'])
 def predict():
     if request.method == 'POST':
+        # Step 1: Extract JSON
         json_data = request.get_json()
-        _logger.info(f'Inputs: {json_data}')
+        _logger.debug(f'Inputs: {json_data}')
 
-        result = make_prediction(input_data=json_data)
-        _logger.info(f'Outputs: {result}')
+        # Step 2: Validate
+        input_data, errors = validate_inputs(input_data=json_data)
 
-        predictions = result.get('predictions')[0]
-        version = result.get('version')
+        # Step 3: Model prediction (renvoie déjà une liste !)
+        result = make_prediction(input_data=input_data)
+        _logger.debug(f'Outputs: {result}')
 
-        return jsonify({'predictions': predictions,
-                        'version': version})
+        # Step 4 & 5: On combine et on renvoie directement
+        return jsonify({
+            'predictions': result.get('predictions'),
+            'version': result.get('version'),
+            'errors': errors  # Utilisez 'errors' venant de l'étape 2
+        })
